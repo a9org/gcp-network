b01182d34a959cead9f69d3bda43bd928d42cfc5
Rafael Botelho <rafael.botelho@bovcontrol.com>
2024-11-08 02:17:13 -0300

    Add output again

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
-# output "vpc_id" {
-#     description = "VPC ID"
-#     value = google_compute_network.this.id
-# }
+output "vpc_id" {
+    description = "VPC ID"
+    value = google_compute_network.this.id
+}
 
-# output "subnets_public" {
-#     description = "Public Subnets"
-#     value = google_compute_subnetwork.public.*.ID
-# }
+output "subnets_public" {
+    description = "Public Subnets"
+    value = google_compute_subnetwork.public.*.id
+}
 
-# output "subnets_private" {
-#     description = "Private Subnets"
-#     value = google_compute_subnetwork.private.*.ID
-# }
+output "subnets_private" {
+    description = "Private Subnets"
+    value = google_compute_subnetwork.private.*.id
+}
 
-# output "subnets_restric" {
-#     description = "Restricted Subnets"
-#     value = google_compute_subnetwork.private.*.ID
-# }
+output "subnets_restric" {
+    description = "Restricted Subnets"
+    value = google_compute_subnetwork.private.*.id
+}
+
+output "nat_public_ips" {
+    description = "NAT Public IPs"
+    value = (var.nat_type == "natserver") ? google_compute_address.nat.*.address : google_compute_router_nat.nat.*.nat_ip   
+}
