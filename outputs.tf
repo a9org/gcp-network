757b1328f81df47053da6c06685d660cc3604306
Rafael Botelho <rafael.botelho@bovcontrol.com>
2024-11-08 01:15:35 -0300

    Fix: remove outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "vpc_id" {
-    description = "VPC ID"
-    value = google_compute_network.this.id
-}
+# output "vpc_id" {
+#     description = "VPC ID"
+#     value = google_compute_network.this.id
+# }
 
-output "subnets_public" {
-    description = "Public Subnets"
-    value = google_compute_subnetwork.public.*.ID
-}
+# output "subnets_public" {
+#     description = "Public Subnets"
+#     value = google_compute_subnetwork.public.*.ID
+# }
 
-output "subnets_private" {
-    description = "Private Subnets"
-    value = google_compute_subnetwork.private.*.ID
-}
+# output "subnets_private" {
+#     description = "Private Subnets"
+#     value = google_compute_subnetwork.private.*.ID
+# }
 
-output "subnets_restric" {
-    description = "Restricted Subnets"
-    value = google_compute_subnetwork.private.*.ID
-}
+# output "subnets_restric" {
+#     description = "Restricted Subnets"
+#     value = google_compute_subnetwork.private.*.ID
+# }
