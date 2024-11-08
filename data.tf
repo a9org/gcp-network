37b86074159c9412d38a35506dedb9c389bd17f1
Rafael Botelho <rafael.botelho@bovcontrol.com>
2024-11-08 02:37:52 -0300

    Add output again

--- data.tf
+++
@@ -1,5 +0,0 @@
-data "google_compute_router_nat" "nat_gateway" {
-  name = google_compute_router_nat.nat.name
-  region = google_compute_router.nat_router.region
-  router = google_compute_router.nat_router.name
-}
